9a509896f045629c16cfca0d0948c0842ec59358
Emir da Ponte <emir.daponte@gmail.com>
2020-09-25 21:17:05 -0300

    deploy

---
+++ precache-manifest.dee348e00a076eb50e314a37f0be66fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d284a3a8a93e413cfec2",
+    "url": "/css/app.0d8a5314.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "809a978645ddf946de328c114cb23f9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d284a3a8a93e413cfec2",
+    "url": "/js/app.a6c164d2.js"
+  },
+  {
+    "revision": "017f6b672da5115b2d34",
+    "url": "/js/chunk-vendors.539b7635.js"
+  },
+  {
+    "revision": "47faeccf606f01feb16e987d9089f22d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
